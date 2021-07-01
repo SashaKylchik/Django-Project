4d63a0c31e740aa37184a50a9a0e75b725e1ff73
AlexandraKara <awesomekylchik@yandex.ru>
2021-07-01 20:55:18 +0300

    Defined template filters with capital letters and displaying the current date in the template

--- products/views.py
+++ products/views.py
@@ -7,7 +7,8 @@ def index(request):
 
     context = {
         'title': 'GeekShop',
-        'header': 'GeekShop Store'
+        'header': 'GeekShop Store',
+        'username': 'Саша К'
         
     }
 
@@ -17,7 +18,15 @@ def products(request):
 
     context = {
         'title': 'GeekShop - Каталог',
-        'header': 'GeekShop'
+        'header': 'GeekShop',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'description':'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.', 'price': '6 090,00 руб.'},
+            {'name': 'Синяя куртка The North Face', 'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.', 'price': '6 090,00 руб.'},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'description': 'Материал с плюшевой текстурой. Удобный и мягкий.','price': '3 390,00 руб.'},
+            {'name': 'Черный рюкзак Nike Heritage', 'description': 'Плотная ткань. Легкий материал.', 'price': '2 340,00 руб.'},
+            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'description': 'Гладкий кожаный верх. Натуральный материал.',  'price': '13 590,00 руб.'},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.', 'price': '12 890,00 руб.'}
+        ]
     }
 
     return render(request, 'products/products.html', context)
