68d1d49ac5ddd5b26d10ab0ebee87332ad401ae5
AlexandraKara <awesomekylchik@yandex.ru>
2021-07-01 23:53:49 +0300

    added image link in template

--- products/views.py
+++ products/views.py
@@ -20,12 +20,12 @@ def products(request):
         'title': 'GeekShop - Каталог',
         'header': 'GeekShop',
         'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'description':'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.', 'price': '6 090,00 руб.'},
-            {'name': 'Синяя куртка The North Face', 'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.', 'price': '6 090,00 руб.'},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'description': 'Материал с плюшевой текстурой. Удобный и мягкий.','price': '3 390,00 руб.'},
-            {'name': 'Черный рюкзак Nike Heritage', 'description': 'Плотная ткань. Легкий материал.', 'price': '2 340,00 руб.'},
-            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'description': 'Гладкий кожаный верх. Натуральный материал.',  'price': '13 590,00 руб.'},
-            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.', 'price': '12 890,00 руб.'}
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'description':'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.', 'price': '6 090,00 руб.', 'image':'vendor/img/products/Adidas-hoodie.png'},
+            {'name': 'Синяя куртка The North Face', 'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.', 'price': '6 090,00 руб.','image':'vendor/img/products/Blue-jacket-The-North-Face.png'},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'description': 'Материал с плюшевой текстурой. Удобный и мягкий.','price': '3 390,00 руб.','image':'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png'},
+            {'name': 'Черный рюкзак Nike Heritage', 'description': 'Плотная ткань. Легкий материал.', 'price': '2 340,00 руб.','image':'vendor/img/products/Black-Nike-Heritage-backpack.png'},
+            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'description': 'Гладкий кожаный верх. Натуральный материал.',  'price': '13 590,00 руб.', 'image':'vendor/img/products/Black-Dr-Martens-shoes.png'},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.', 'price': '12 890,00 руб.','image':'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'}
         ]
     }
 
